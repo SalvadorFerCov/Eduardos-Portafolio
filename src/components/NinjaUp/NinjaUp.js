c40a6d2712be655ad5df1fcd88c02f5629f00e3e
Salvador Fernandez Covarrubias <salvador.fer.cov@gmail.com>
2021-07-11 18:36:22 +0200

    added ninja up page, created common PicText component, server routing to be researched

--- src/components/NinjaUp/NinjaUp.js
+++ src/components/NinjaUp/NinjaUp.js
@@ -3,8 +3,64 @@ import React from "react";
 import styles from "./NinjaUp.module.css";
 import PicText from "../PicText/PicText";
 
+const renderNinjaUpGamePlay = () => (
+  <>
+    <h2>Gameplay</h2>
+    <ul>
+      <li>
+        Dodge or try to hit a flurry of cool enemies from ninjas to fun UFOs.
+      </li>
+      <li>
+        Draw lines for your ninja to bounce off of, trying to go up and reach
+        the best height possible in an endless sea of challenges.
+      </li>
+      <li>
+        The shorter the rope, the higher and faster you will jump. Will you take
+        the risk?
+      </li>
+      <li>
+        Discover cool ninja power-ups  that will help you reach incredible
+        heights.
+      </li>
+      <li>Unlock all the fun jump master characters and new pixel costumes.</li>
+      <li>Challenge your friends or compete in global leaderboards.</li>
+    </ul>
+  </>
+);
+
+const renderNinjaUpGameDesign = () => (
+  <>
+    <h2>Game Design thought process and notes</h2>
+    <p>
+      This was a special case because the game was already done by another team
+      but the studio wanted to relaunch it with more features. The approach we
+      took was to look into what made it special and just add more ways for that
+      special component to shine. We identified 2 special components: the
+      jumping mechanic and costumes.
+    </p>
+    <ul>
+      <li>
+        Jumping mechanic: We wanted to give the player more ways to keep jumping
+        and trying to reach new heights so we came up with the idea of power
+        ups. Extra lifes, invincibility for a short amount of time, hyper jump,
+        among others. We also added a EX jump after consecutive successful risky
+        jumps to reward users with high skill.{" "}
+      </li>
+      <li>
+        Costumes: The original had 10 costumes that mainly varied from color. We
+        decided to include 20+ costumes with different themes like animal,
+        job-related, music genre and even some pop-culture references. We then
+        decided to give the original 10 costumes a challenge associated to
+        unlock them. You purchased a random drop using Coins gained from various
+        sources: during your run, by hitting a special enemy and at the end of
+        your run based on your performance.
+      </li>
+    </ul>
+  </>
+);
+
 const NinjaUp = () => (
-  <div className={styles.NinjaUp}>
+  <div>
     <div className={styles.content}>
       <h1> Ninja Up! </h1>
       <p>Jump your way into space with the most daring and cool pixel ninja!</p>
@@ -12,7 +68,7 @@ const NinjaUp = () => (
         Join the endless run action. Draw lines to create new springboards for
         your ninja to jump from so he can bounce his way up and up!​​​​​​​
       </p>
-      <PicText />
+      <PicText> {renderNinjaUpGamePlay()}</PicText>
       <div>
         <h1> Roles and responsibilities</h1>
         <p>
@@ -31,6 +87,7 @@ const NinjaUp = () => (
           the costumes.
         </p>
       </div>
+      <PicText picFirst> {renderNinjaUpGameDesign()}</PicText>
     </div>
   </div>
 );
--- src/components/PicText/PicText.js
+++ src/components/PicText/PicText.js
@@ -1,10 +1,12 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import styles from "./PicText.module.css";
+import classNames from "classnames/bind";
 
-// TO-DO make reusable
-const PicText = () => (
-  <div className={styles.PicText}>
+const cx = classNames.bind(styles);
+
+const PicText = ({ children, picFirst }) => (
+  <div className={cx(styles.PicText, { picFirst })}>
     <div
       style={{
         backgroundImage: `url(${process.env.PUBLIC_URL}/melee.jpg`,
@@ -13,35 +15,17 @@ const PicText = () => (
       }}
       className={styles.background}
     />
-    <div className={styles.content}>
-      <h2>Gameplay</h2>
-      <ul>
-        <li>
-          Dodge or try to hit a flurry of cool enemies from ninjas to fun UFOs.
-        </li>
-        <li>
-          Draw lines for your ninja to bounce off of, trying to go up and reach
-          the best height possible in an endless sea of challenges.
-        </li>
-        <li>
-          The shorter the rope, the higher and faster you will jump. Will you
-          take the risk?
-        </li>
-        <li>
-          Discover cool ninja power-ups  that will help you reach incredible
-          heights.
-        </li>
-        <li>
-          Unlock all the fun jump master characters and new pixel costumes.
-        </li>
-        <li>Challenge your friends or compete in global leaderboards.</li>
-      </ul>
-    </div>
+    <div className={styles.content}>{children}</div>
   </div>
 );
 
-PicText.propTypes = {};
+PicText.propTypes = {
+  children: PropTypes.node.isRequired,
+  picFirst: PropTypes.bool,
+};
 
-PicText.defaultProps = {};
+PicText.defaultProps = {
+  picFirst: false,
+};
 
 export default PicText;
